7e534b33b01e90441d0442f5201cbee304ae35c1
Abishekthealone <shadowalone02@gmail.com>
2026-03-20 13:40:25 +0530

    first commit

---
+++ src/test/java/com/badminton/winzz/WinzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badminton.winzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
